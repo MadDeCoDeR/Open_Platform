8ba11ab0fd9011a158d7f38a65195fcf2b28f43d
MadDeCoDeR <gkwin8ac@hotmail.gr>
2019-02-09 11:16:41 +0200

    Add project files.

---
+++ buildlinux/CMakeFiles/OpenPlatform.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenPlatform.dir/OpenPlatform.cpp.o"
+  "OpenPlatform.pdb"
+  "OpenPlatformx86_64.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenPlatform.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ buildlinux/CMakeFiles/OpenPlatform.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for OpenPlatform.
+# This may be replaced when dependencies are built.
